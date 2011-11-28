8bb8891c962a67ef0c8183ce2bca18982a61e094
Paul Wollaston <paulw@hardmilk.com>
2011-11-28 15:14:08 +0000

    Update updater to build dependencies (test)

--- updater/update.sh
+++ updater/update.sh
@@ -69,3 +69,11 @@ else
 	#No update available
 	logOut "Update is not available.  Current version is $(cat ${srcroot}version)"
 fi
+#Let's compile some required binaries.  We do this every time, just incase.
+#Compile FTGL
+logOut "Compiling and installing FTGL..."
+cd ${srcroot}ext-libs/ftgl > /dev/null 2>&1
+./autogen.sh > /dev/null 2>&1
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
