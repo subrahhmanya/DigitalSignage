012d033a2fc6a8d14490f3ef30110e6e8010abec
Paul Wollaston <paulw@hardmilk.com>
2011-11-29 15:03:58 +0000

    Update builder, pull new version.

--- updater/builder.sh
+++ updater/builder.sh
@@ -13,13 +13,13 @@ function logOut {
 
 #Let's compile some required binaries.  We do this every time, just incase.
 #Compile FTGL
-#logOut "Compiling and installing FTGL..."
-#cd ${srcroot}ext-libs/ftgl > /dev/null 2>&1
-#./autogen.sh > /dev/null 2>&1
-#./configure > /dev/null 2>&1
-#make > /dev/null 2>&1
-#sudo make install > /dev/null 2>&1
-logOut "Skipping FTGL Compilation - already installed."
+logOut "Compiling and installing FTGL..."
+cd ${srcroot}ext-libs/ftgl > /dev/null 2>&1
+./autogen.sh > /dev/null 2>&1
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
+#logOut "Skipping FTGL Compilation - already installed."
 
 #Compile CutyCapt
 logOut "Compiling CutyCapt..."
@@ -30,6 +30,7 @@ cp CutyCapt ${scrroot} > /dev/null 2>&1
 rm CutyCapt > /dev/null 2>&1
 
 #Compile infodisplay
+logOut "Compiling InfoDisplay..."
 cd ${srcroot}src > /dev/null 2>&1
 make > /dev/null 2>&1
 killall infodisplay > /dev/null 2>&1
@@ -37,4 +38,7 @@ cp infodisplay ${scrroot} > /dev/null 2>&1
 rm infodisplay > /dev/null 2>&1
 
 # Load Application
+logOut "Launching InfoDisplay..."
 env DISPLAY=:0 ${scrroot}infodisplay &
+
+logOut "Complete!"
