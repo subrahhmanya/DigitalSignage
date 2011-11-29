86e0db2c39e1ceaca4e11c52eaa0cb109deaabda
Paul Wollaston <paulw@hardmilk.com>
2011-11-29 16:33:03 +0000

    Run check - remove user check again...

--- updater/runcheck.sh
+++ updater/runcheck.sh
@@ -3,9 +3,7 @@
 # Static Variables
         scrroot="/screen/"
 
-if [ ! -z "$(w -h screen)" ] ; then
-	if [ ! $(pidof infodisplay) ] ; then
-		cd ${scrroot}
-		env DISPLAY=:0 ${scrroot}infodisplay &
-	fi
+if [ ! $(pidof infodisplay) ] ; then
+	cd ${scrroot}
+	env DISPLAY=:0 ${scrroot}infodisplay &
 fi
