4d192e307bf50cb19861666cedb32579c22ba81d
Paul Wollaston <paulw@hardmilk.com>
2011-12-02 09:27:09 +0000

    Bit of housekeeping.

--- src/infodisplay.cpp
+++ src/infodisplay.cpp
@@ -19,13 +19,13 @@
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 const int SCREEN_BPP = 24;
- 
+
 SDL_Surface* screen = NULL;
 TTF_Font *fntCGothic;
 
 typedef unsigned char byte;
 
-void drawText(const char *text, 
+void drawText(const char *text,
                       TTF_Font *font,
                       SDL_Color color,
                       SDL_Rect *location);
@@ -50,29 +50,13 @@ SDL_Surface* setColorKeyOrg(SDL_Surface* s, Uint32 maskColor)
     return s;
 }
 
-void render() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	SDL_Color iro;
-	SDL_Rect position;
-
-	iro.r = 0;
-	iro.g = 0;
-	iro.b = 0;
-	position.x = 0;
-	position.y = 0;
-	drawText("Orbital", fntCGothic, iro, &position);
- 
-	SDL_GL_SwapBuffers();
-}
-
 int nextpoweroftwo(int x)
 {
 	double logbase2 = log(x) / log(2);
 	return round(pow(2,ceil(logbase2)));
 }
 
-void drawText(const char *text, 
+void drawText(const char *text,
                       TTF_Font *font,
                       SDL_Color color,
                       SDL_Rect *location)
@@ -82,14 +66,14 @@ void drawText(const char *text,
 	SDL_Rect rect;
 	int w,h;
 	GLuint texture;
-	
+
 	/* Use SDL_TTF to render our text */
 	initial = TTF_RenderUTF8_Blended(font, text, color);
-	
+
 	/* Convert the rendered text to a known format */
 	w = nextpoweroftwo(initial->w);
 	h = nextpoweroftwo(initial->h);
-	
+
 	/* We need to allocate some memory now for out buffer */
 	unsigned char *pixels;
 	pixels = (unsigned char*) malloc(4 * w * h);
@@ -119,12 +103,12 @@ void drawText(const char *text,
 	/* Tell GL about our new texture */
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, w, h, 0, GL_RGBA, 
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, w, h, 0, GL_RGBA,
 			GL_UNSIGNED_BYTE, intermediary->pixels );
-	
+
 	/* GL_NEAREST looks horrible, if scaled... */
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	/* prepare to render our texture */
 	glEnable(GL_TEXTURE_2D);
@@ -136,22 +120,22 @@ void drawText(const char *text,
 		/* Recall that the origin is in the lower-left corner
 		   That is why the TexCoords specify different corners
 		   than the Vertex coors seem to. */
-		glTexCoord2f(0.0f, 1.0f); 
+		glTexCoord2f(0.0f, 1.0f);
 			glVertex2f(location->x    , location->y);
-		glTexCoord2f(1.0f, 1.0f); 
+		glTexCoord2f(1.0f, 1.0f);
 			glVertex2f(location->x + w, location->y);
-		glTexCoord2f(1.0f, 0.0f); 
+		glTexCoord2f(1.0f, 0.0f);
 			glVertex2f(location->x + w, location->y + h);
-		glTexCoord2f(0.0f, 0.0f); 
+		glTexCoord2f(0.0f, 0.0f);
 			glVertex2f(location->x    , location->y + h);
 	glEnd();
 	/* Bad things happen if we delete the texture before it finishes */
 	glFinish();
-	
+
 	/* return the deltas in the unused w,h part of the rect */
 	location->w = initial->w;
 	location->h = initial->h;
-	
+
 	/* Clean up */
 	SDL_FreeSurface(initial);
 	SDL_FreeSurface(intermediary);
@@ -161,20 +145,20 @@ void drawText(const char *text,
 bool init() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	TTF_Init();
- 
+
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE,   8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,  8);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
- 
+
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  16);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
- 
+
 	SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,   8);
 	SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,  8);
 	SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE, 8);
- 
+
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
 	SDL_putenv("SDL_VIDEO_WINDOW_POS=0,0");
 	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_FULLSCREEN | SDL_HWSURFACE | SDL_OPENGL);
@@ -185,7 +169,7 @@ bool init() {
 	}
 
 	SDL_ShowCursor(SDL_DISABLE); 
- 
+
 	glClearColor(1, 1, 1, 1);
 	glClearDepth(1.0f);
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -200,32 +184,48 @@ bool init() {
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	glPushMatrix();
 	glLoadIdentity();
- 
+
 	SDL_WM_SetCaption("Info Display", NULL);
- 
+
 	fntCGothic = TTF_OpenFont( "/screen/fonts/cgothic.ttf", 48 );
 	if(fntCGothic == NULL) {
 		fprintf( stderr, "Failed when loading Font: %s\n",
 		SDL_GetError( ) );
 		return false;
 	}
- 
 	return true;
 }
- 
+
+void render() {
+	// This is the main rendering function.  In here, we call all required information and pull what we need.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	SDL_Color iro;
+	SDL_Rect position;
+
+	iro.r = 0;
+	iro.g = 0;
+	iro.b = 0;
+	position.x = 0;
+	position.y = 0;
+	drawText("Orbital", fntCGothic, iro, &position);
+
+	SDL_GL_SwapBuffers();
+}
+
 int main( int argc, char* argv[] ) {
 	if(init() == false) {
 		return 1;
 	}
- 
+
 	render();
- 
+
 	system("sleep 5");
- 
+
 	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();   
+	glPopMatrix();
 	glMatrixMode(GL_MODELVIEW);
-	glPopMatrix();	
+	glPopMatrix();
 
 	SDL_FreeSurface(screen);
 	TTF_CloseFont(fntCGothic);
