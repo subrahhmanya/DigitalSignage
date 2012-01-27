13edfc05fd2d5bb0c5eba64f7ec0e53067fb8d71
Paul Wollaston <paulw@hardmilk.com>
2012-01-27 11:28:35 +0000

    Updater changes

--- updater/puTasks.sh
+++ updater/puTasks.sh
@@ -18,18 +18,21 @@ cd ${srcroot} > /dev/null 2>&1
 make > /dev/null 2>&1
 
 #Create Directory Structure (if not already done)
-mkdir -p ${scrroot}boards/1
-mkdir -p ${scrroot}boards/2
-mkdir -p ${scrroot}boards/3
+logOut "Creating structure for Boards..."
+mkdir -p ${scrroot}boards/1 > /dev/null 2>&1
+mkdir -p ${scrroot}boards/2 > /dev/null 2>&1
+mkdir -p ${scrroot}boards/3 > /dev/null 2>&1
 
 #Move new binary and associated files...
-rm ${scrroot}infodisplay
+logOut "Removing old infodisplay files..."
+rm ${scrroot}infodisplay > /dev/null 2>&1
 cp infodisplay ${scrroot} > /dev/null 2>&1
 rm infodisplay > /dev/null 2>&1
-rm -rf ${scrroot}fonts
-rm -rf ${scrroot}textures
-cp -a fonts ${scrroot}
-cp -a textures ${scrroot}
+rm -rf ${scrroot}fonts > /dev/null 2>&1
+rm -rf ${scrroot}textures > /dev/null 2>&1
+logOut "Copying new infodisplay files..>"
+cp -a fonts ${scrroot} > /dev/null 2>&1
+cp -a textures ${scrroot} > /dev/null 2>&1
 cp ${srcroot}updater/runcheck.sh ${scrroot} > /dev/null 2>&1
 
 # Load Application
