a5630e268b7050e6e5a1a5870dc441b1f504e61e
Paul Wollaston <paulw@hardmilk.com>
2012-05-25 11:47:29 +0100

    Allow the use of a Config file instead of launch variables being hard-coded. (Multi-Machine Support)

--- main.cpp
+++ main.cpp
@@ -13,28 +13,59 @@ int main(int argc, char* argv[])
 	srand(time(NULL)); /* Randomise Time Seed */
 	Signage signage;
 
-	signage.Init("Digital Signage", 1280, 720, 32, FileExists("/screen/full"));
-
-	while (signage.Running())
+	printf("Loading Configuration....");
+	/* Check for valid Configuration Files and Contents */
+	char tFName[128];
+	sprintf(tFName, "/screen/config.ini");
+	if (FileExists(tFName))
 	{
-		signage.HandleEvents(&signage);
-		signage.Update();
-		signage.Draw();
+		/* Found a config gile, parse it... */
+		CIniFile ini;
+		ini.Load(tFName);
+		CIniSection* pSection = ini.GetSection("GeneralSettings");
+		if (pSection)
+		{
+			CIniKey* pKey;
+			if (pSection->GetKey("Enabled"))
+			{
+				if (atoi(ini.GetKeyValue("GeneralSettings", "Enabled").c_str()) == 1)
+				{
+					/* Enabled - Get Configs! */
+					bool tFS = false;
+					int tWX, tWH, tBPP;
+					char tMID[256];
+					if (atoi(ini.GetKeyValue("GeneralSettings", "FullScreen").c_str()) == 1)
+						tFS = true;
+					tWX = atoi(ini.GetKeyValue("GeneralSettings", "SWidth").c_str());
+					tWH = atoi(ini.GetKeyValue("GeneralSettings", "SHeight").c_str());
+					tBPP = atoi(ini.GetKeyValue("GeneralSettings", "BPP").c_str());
+					sprintf(tMID, "%s", ini.GetKeyValue("GeneralSettings", "MID").c_str());
+
+					signage.Init(tMID, tWX, tWH, tBPP, tFS);
+
+					while (signage.Running())
+					{
+						signage.HandleEvents(&signage);
+						signage.Update();
+						signage.Draw();
+					}
+
+					/* Cleanup Engine */
+					signage.Clean();
+
+					/* Sanity */
+					while (signage.Running())
+					{
+						signage.Clean();
+						signage.Update();
+						signage.Draw();
+					}
+
+					printf("OrbitalDigitalSignage has cleanly exited.\n");
+				}
+			}
+		}
 	}
-
-	/* Cleanup Engine */
-	signage.Clean();
-
-	/* Sanity */
-	while (signage.Running())
-	{
-		signage.Clean();
-		signage.Update();
-		signage.Draw();
-	}
-
-	printf("OrbitalDigitalSignage has cleanly exited.\n");
-
 	return 0;
 }
 
