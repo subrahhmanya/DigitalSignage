b5993b6eab40c10acfbe8676edb5de747ed34192
Paul Wollaston <paulw@hardmilk.com>
2012-09-26 12:23:53 +0100

    Ensure we clear get_iplayer prefs at each launch.  We always want fresh configs so we know things work!

--- runcheck.sh
+++ runcheck.sh
@@ -9,5 +9,8 @@ if [ ! $(pidof DigitalSignage) ] ; then
 	killall -9 rtmpdump > /dev/null 2>&1
 	killall -9 get_iplayer > /dev/null 2>&1
 	cd ${scrroot}
+        # Clean out getiPlayer Prefs.
+        ${scrroot}deps/ip/get_iplayer --prefs-clear > /dev/null 2>&1
+        ${scrroot}deps/ip/get_iplayer --refresh > /dev/null 2>&1
 	env DISPLAY=:0 ${scrroot}DigitalSignage & > /dev/null 2>&1
 fi
