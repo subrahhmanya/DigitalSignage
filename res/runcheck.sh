bb1996cbaed4a45a9e7ea9b49518626236aa95b7
Paul Wollaston <paulw@hardmilk.com>
2012-09-27 09:59:06 +0100

    Update DPKG building scripts & tools.

---
+++ res/runcheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Run
+# Static Variables
+	scrroot="/opt/digitalsignage/"
+
+if [ ! $(pidof DigitalSignage) ] ; then
+	# Kill any other instance of mplayer/rtmpdump/get_iplayer just incase already running.
+
+	if ps ax | grep -v "grep" | grep "mplayer" > /dev/null; then
+	        killall -9 mplayer > /dev/null 2>&1
+	fi
+
+	if ps ax | grep -v "grep" | grep "rtmpdump" > /dev/null; then
+	        killall -9 rtmpdump > /dev/null 2>&1
+	fi
+
+	if ps ax | grep -v "grep" | grep "get_iplayer" > /dev/null; then
+	        killall -9 get_iplayer > /dev/null 2>&1
+	fi	cd ${scrroot}
+        # Clean out getiPlayer Prefs.
+        ${scrroot}deps/ip/get_iplayer --prefs-clear > /dev/null 2>&1
+        ${scrroot}deps/ip/get_iplayer --refresh > /dev/null 2>&1
+	env DISPLAY=:0 ${scrroot}DigitalSignage & > /dev/null 2>&1
+fi
